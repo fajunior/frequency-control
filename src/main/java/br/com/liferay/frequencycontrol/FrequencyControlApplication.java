bf8555bfc28349a827947d4b764d56a91a427cd9
Francisco <fcoajunior@gmail.com>
2019-11-26 18:16:59 -0300

    Intial commit

---
+++ src/main/java/br/com/liferay/frequencycontrol/FrequencyControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.liferay.frequencycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequencyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrequencyControlApplication.class, args);
+	}
+
+}
